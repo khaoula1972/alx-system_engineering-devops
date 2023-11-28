dbc08bcc4c533a19e7973de50230d66e7f00024a
khaoula1972 <khaoula.nakkach@gmail.com>
2023-11-28 16:08:26 +0100

    Install Flask with Puppet

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# Install Flask
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/local/bin/:/usr/bin/',
+  require => Package['python3-pip'],
+}
